b4ac2f59ebd1e5d22c8830251bfe0f893e15f29f
greysar <greysar08@gmail.com>
2023-12-18 23:05:35 +0500

    feat: add isPowerTwo

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -338,8 +338,9 @@ function getSumOfDigits(num) {
  *   16  => true
  *   15  => false
  */
-function isPowerOfTwo(/* num */) {
-  throw new Error('Not implemented');
+function isPowerOfTwo(num) {
+  if (Math.log2(num) % Math.round(Math.log2(num)) === 0) return true;
+  return false;
 }
 
 /**
